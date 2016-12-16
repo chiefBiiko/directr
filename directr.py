f0674ea12de878ee168a9deb98074ea704531fe5
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2016-12-16 17:51:12 +0100

    need to internalize unicode and pipe stdout conditionally on arg log

--- directr.py
+++ directr.py
@@ -1,10 +1,7 @@
 # Get an eye for your src files
 
-# TODO: rewrite internal string comparisons with unicode
-        # n need 2 findaway 2 encode unicode pathnames from walking soundly
-            # cant handle ae, oe, ue currently
-        # blaming os.path.join() in walkings g_src.extend(...).. so far..
-        
+# TODO: internalize unicode; redirect stdout to terminal or file depending on arg log
+
 import sys, os, time, datetime, subprocess
 
 def sd4src(dirs=[os.getcwd()], mode=['r', 'py', 'js'], log=False, walk=False, display=False, delay=10.):
@@ -19,7 +16,7 @@ def sd4src(dirs=[os.getcwd()], mode=['r', 'py', 'js'], log=False, walk=False, di
         mode (list): specifying the types of src files to gather, allowed:
         'r', 'py', 'js', 'c', 'java', 'markup', 'markdown', 'css', 'txt',
         'misc'
-        log (bool): if True, sd_4src saves a .txt log file in cwd
+        log (bool): if True, sd4src saves a .txt log file in cwd
         walk (bool): if True, recursively walk down dirs[i]
         display (bool): if True, display src files in notepad
         delay (float): if display, display period for each src file in s
@@ -30,18 +27,18 @@ def sd4src(dirs=[os.getcwd()], mode=['r', 'py', 'js'], log=False, walk=False, di
     Examples:
         >>> import directr as dr
         # accepting all defaults, scanning cwd
-        >>> dr.sd_4src()
+        >>> dr.sd4src()
         ['C:\\foo.R', 'C:\\bar.py']
         # searching multiple directories
-        >>> dr.sd_4src(dirs=[os.getcwd(), 'C:\\taskschdlr'], mode='r')
+        >>> dr.sd4src(dirs=[os.getcwd(), 'C:\\taskschdlr'], mode='r')
         ['C:\\foo.R', 'C:\\taskschdlr\\task.R']
         # full-blown scan using walk
-        >>> dr.sd_4src(dirs=['C:\\'], log=True, walk=True)
+        >>> dr.sd4src(dirs=['C:\\'], log=True, walk=True)
         ['C:\\foo.R', 'C:\\bar.py', 'C:\\taskschdlr\\task.R', 'C:\\z\\oo.js']
     """
     # setup
     g_src = []
-    enkot = sys.stdout.encoding
+    mbcs = sys.getfilesystemencoding()
     exts = {'r': ['.r', '.rmd'], 'py': ['.py'], 'js': ['.js'],
             'c': ['.c', '.cpp', '.cxx', '.h', '.hpp', '.hxx'],
             'java': ['.java', '.jar', '.jad'],
@@ -53,7 +50,7 @@ def sd4src(dirs=[os.getcwd()], mode=['r', 'py', 'js'], log=False, walk=False, di
     dtst = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S') + ' UTC'
     print('#'*79 + '\n{} -  Starting search 4 {} src files...\n'.format(dtst, mode))
     if log:
-        with open(os.path.join(os.getcwd(), 'sd_4src_log.txt'), 'a') as f:
+        with open(os.path.join(os.getcwd(), 'sd4src_log.txt'), 'a') as f:
             f.write('#'*79 + '\n{} - Starting search 4 {} src files...\n'.format(dtst, mode))
     # gathering src files for each dir
     if not walk:
@@ -63,7 +60,7 @@ def sd4src(dirs=[os.getcwd()], mode=['r', 'py', 'js'], log=False, walk=False, di
             g_src.extend([os.path.join(d, fl) for fl in d_src])
             print('#'*79 + '\nDir: {}\nSrc: {}\n'.format(d, d_src))
             if log:
-                with open(os.path.join(os.getcwd(), 'sd_4src_log.txt'), 'a') as f:
+                with open(os.path.join(os.getcwd(), 'sd4src_log.txt'), 'a') as f:
                     f.write('#'*79 + '\nDir: {}\nSrc: {}\n'.format(d, d_src))
             # calling notepad
             if display and len(d_src) > 0:
@@ -84,10 +81,12 @@ def sd4src(dirs=[os.getcwd()], mode=['r', 'py', 'js'], log=False, walk=False, di
                     drs.remove('.git')
                 d_src = [f for f in fls if f.lower().endswith(md)]
                 g_src.extend([os.path.join(rt, fl) for fl in d_src])
-                print('#'*79 + '\nDir: {}\nSrc: {}\n'.format(rt.encode(enkot, 'replace'), [ds.encode(enkot, 'replace') for ds in d_src]))
+                d_enc = [ds.encode(mbcs, 'replace') for ds in d_src]
+                rt_enc = rt.encode(mbcs, 'replace')
+                print('#'*79 + '\nDir: {}\nSrc: {}\n'.format(rt_enc, d_enc))
                 if log:
-                    with open(os.path.join(os.getcwd(), 'sd_4src_log.txt'), 'a') as f:
-                        f.write('#'*79 + '\nDir: {}\nSrc: {}\n'.format(rt.encode(enkot, 'replace'), [ds.encode(enkot, 'replace') for ds in d_src]))
+                    with open(os.path.join(os.getcwd(), 'sd4src_log.txt'), 'a') as f:
+                        f.write('#'*79 + '\nDir: {}\nSrc: {}\n'.format(rt_enc, d_enc))
                 # calling notepad
                 if display and len(d_src) > 0:
                     print('Displaying src files...\n')
@@ -95,13 +94,13 @@ def sd4src(dirs=[os.getcwd()], mode=['r', 'py', 'js'], log=False, walk=False, di
                         pr = subprocess.Popen(['C:\\Windows\\System32\\notepad.exe', os.path.join(rt, s)])
                         time.sleep(delay)
                         pr.terminate()
-    # exit --- main bug seems to relate 2 encoding error within list comprehension in format stmt of print()
-    g_enc = [gs.encode(enkot, 'replace') for gs in g_src]
+    # exit --- cant show encoded strings displaying and returning unicode on exit
+    #g_enc = [gs.encode(mbcs, 'replace') for gs in g_src]
     dtst = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S') + ' UTC'
-    print('#'*79 + '\n{}  - Done.\nAll src files: {}\n'.format(dtst, [gs.encode(enkot, 'replace') for gs in g_src]))
+    print('#'*79 + '\n{}  - Done.\nAll src files: {}\n'.format(dtst, g_src))
     if log:
-        with open(os.path.join(os.getcwd(), 'sd_4src_log.txt'), 'a') as f:
-            f.write('#'*79 + '\n{} - Done.\nAll src files: {}\n'.format(dtst, [gs.encode(enkot, 'replace') for gs in g_src]))
+        with open(os.path.join(os.getcwd(), 'sd4src_log.txt'), 'a') as f:
+            f.write('#'*79 + '\n{} - Done.\nAll src files: {}\n'.format(dtst, g_src))
     if display and len(g_src) > 0: 
         ui = raw_input('Reopen all notepad windows now? [y/n] ')
         if 'y' in ui:
